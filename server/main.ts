4d797092b7affbb11a8e7d53854f6e917d90c7b2
James Canning <jamesocanning@gmail.com>
2023-12-21 15:38:22 +0000

    add templating + static assets

--- server/main.ts
+++ server/main.ts
@@ -1,11 +1,29 @@
 import Fastify from 'fastify';
+import view from '@fastify/view';
+import fstatic from '@fastify/static';
+import * as nunjucks from 'nunjucks';
+import * as path from 'node:path';
 
 const fastify = Fastify({
   logger: true
 })
 
+console.log( path.join(__dirname, "../src/views"))
+
+fastify.register(view, {
+  engine: {
+    nunjucks,
+  },
+  root: path.join(__dirname, "../server/views")
+});
+
+fastify.register(fstatic, {
+  root: path.join(__dirname, '../public'),
+  prefix: '/public/', // optional: default '/'
+})
+
 fastify.get('/', function (request, reply) {
-  reply.send({ purpose: 'Web Wars!' })
+  reply.view('base/main.html')
 })
 
 fastify.listen({ port: 3000 }, function (err, address) {
